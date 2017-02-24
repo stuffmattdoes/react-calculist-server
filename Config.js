15abffad132e5505eb08b48d8fd61cce523c044f
Matthew J. Morrison <m.james.morrison00@gmail.com>
2017-02-23 21:13:48 -0500

    Reworked dev dependencies

---
+++ Config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'database': 'mongodb://matt_admin:9hx75737@ds133398.mlab.com:33398/calculist',
+    'port' : process.env.PORT || process.env.SERVER_PORT || 8001,
+    'secret': 'noodles is the best cat X(*I$0isdj0fif9op2FHKL&*IslWP}'
+}
